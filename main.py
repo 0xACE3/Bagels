6b5243b7bc9c010bcf5c5678b8e177f51d47c8a9
0xharis <94894153+MetaCircuits@users.noreply.github.com>
2022-04-18 07:48:59 +0500

    changes made

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+"""
+Bagels, a deductive logic game.
+By 0xHaris
+I am thinking of a 3-digit number. Try your best to guess what it is.
+Here are some clues:
+When I say:         that means:
+Pico                One digit is correct, but in the wrong position.
+Fermi               One digit is correct, but in the right position.
+Bagels              No digit is correct.
+"""
 
+
+def main():
+    pass
